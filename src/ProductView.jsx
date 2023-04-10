22ccd73ecee6912cf4c2e74726a5271f4504e0dd
Suresh Hacker <124077353+Hackersuresh08@users.noreply.github.com>
2023-04-10 22:12:02 +0530

    Add files via upload

---
+++ src/ProductView.jsx
@@ -0,0 +1,12 @@
+const ProductView = ()=>{
+return<>
+<ul>
+    <li><img src= "C:\Windows\System32\cmd.exe"></img>Mobile</li>
+    <li>Laptop</li>
+    <li>Home Appliances</li>
+</ul>
+
+</>
+}
+
+export default ProductView;
